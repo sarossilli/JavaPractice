c52ea4423341c0fcd1eb85c139a791308c82e256
Sam Rossilli <Sarossilli@gmail.com>
2022-01-22 22:12:45 -0700

    hello world

---
+++ src/JavaPracticeApp.java
@@ -0,0 +1,6 @@
+public class JavaPracticeApp {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Repo is for practicing data structures/algos in java");
+    }
+}
